6861d7b895417e5ceb8e214cb1636195b75b1bd4
incognia <incognia@gmail.com>
2023-08-09 03:30:19 -0600

    4°, 5° y 6° de primaria

--- Primaria/pdf.sh
+++ Primaria/pdf.sh
@@ -4,5 +4,6 @@ mv $(find . -type f -name "P1*.pdf") PDF/01/
 mv $(find . -type f -name "P2*.pdf") PDF/02/
 mv $(find . -type f -name "P3*.pdf") PDF/03/
 mv $(find . -type f -name "P4*.pdf") PDF/04/
+mv $(find . -type f -name "P0*.pdf") PDF/04/
 mv $(find . -type f -name "P5*.pdf") PDF/05/
 mv $(find . -type f -name "P6*.pdf") PDF/06/
